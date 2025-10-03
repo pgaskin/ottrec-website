ea5fd734d989267c6cfd7dba87427ddbdc624be6
Patrick Gaskin <patrick@pgaskin.net>
2025-10-03 00:33:45 -0400

    internal/pflagx: implement env parsing

--- internal/pflagx/pflagx.go
+++ internal/pflagx/pflagx.go
@@ -2,7 +2,11 @@
 package pflagx
 
 import (
+	"fmt"
 	"log/slog"
+	"os"
+	"strings"
+	"unicode"
 
 	"github.com/spf13/pflag"
 )
@@ -27,4 +31,33 @@ func (fs *FlagSet) LevelP(name, shorthand string, value slog.Level, usage string
 	def.Set(value)
 	pflag.TextVarP(level, name, shorthand, def, usage)
 	return level
+}
+
+func ParseEnv(prefix string) {
+	FlagSetExt(pflag.CommandLine).ParseEnv(prefix)
+}
+
+func (fs *FlagSet) ParseEnv(prefix string) {
+	for _, env := range os.Environ() {
+		if k, v, ok := strings.Cut(env, "="); ok {
+			if s, ok := strings.CutPrefix(k, prefix); ok {
+				n := strings.Map(func(r rune) rune {
+					switch r {
+					case '_':
+						return '-'
+					}
+					return unicode.ToLower(r)
+				}, s)
+				f := pflag.CommandLine.Lookup(n)
+				if f == nil {
+					fmt.Fprintf(fs.FlagSet().Output(), "env %s: unknown flag --%s\n", k, n)
+					continue
+				}
+				if err := f.Value.Set(v); err != nil {
+					fmt.Fprintf(fs.FlagSet().Output(), "env %s: flag --%s: invalid argument: %v\n", k, n, err)
+					os.Exit(2)
+				}
+			}
+		}
+	}
 }
