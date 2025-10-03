9a3a4070833713d7822069d08432cca9895d6001
Patrick Gaskin <patrick@pgaskin.net>
2025-10-03 00:33:33 -0400

    internal/pflagx: implement slog level flag

--- internal/pflagx/pflagx.go
+++ internal/pflagx/pflagx.go
@@ -2,6 +2,8 @@
 package pflagx
 
 import (
+	"log/slog"
+
 	"github.com/spf13/pflag"
 )
 
@@ -13,4 +15,16 @@ func FlagSetExt(fs *pflag.FlagSet) *FlagSet {
 
 func (fs *FlagSet) FlagSet() *pflag.FlagSet {
 	return (*pflag.FlagSet)(fs)
+}
+
+func LevelP(name, shorthand string, value slog.Level, usage string) *slog.LevelVar {
+	return FlagSetExt(pflag.CommandLine).LevelP(name, shorthand, value, usage)
+}
+
+func (fs *FlagSet) LevelP(name, shorthand string, value slog.Level, usage string) *slog.LevelVar {
+	level := new(slog.LevelVar)
+	def := new(slog.LevelVar)
+	def.Set(value)
+	pflag.TextVarP(level, name, shorthand, def, usage)
+	return level
 }
