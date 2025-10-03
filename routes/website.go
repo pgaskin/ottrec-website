0c5d575a3e07b6022af494377a15407945e5a5d1
Patrick Gaskin <patrick@pgaskin.net>
2025-10-03 01:32:11 -0400

    routes: add website/ handler

--- routes/website.go
+++ routes/website.go
@@ -2,10 +2,13 @@ package routes
 
 import (
 	"fmt"
+	"log/slog"
 	"net/http"
 
+	"github.com/a-h/templ"
 	"github.com/pgaskin/ottrec-website/pkg/ottrecidx"
 	"github.com/pgaskin/ottrec-website/static"
+	"github.com/pgaskin/ottrec-website/templates"
 )
 
 type WebsiteConfig struct {
@@ -21,13 +24,65 @@ func Website(cfg WebsiteConfig) (http.Handler, error) {
 		return nil, fmt.Errorf("no data getter specified")
 	}
 
+	base := websiteHandlerBase{
+		Host: cfg.Host,
+		Data: cfg.Data,
+	}
 	mux := http.NewServeMux()
 
 	// TODO: favicon
 	// TODO: fonts
 	// TODO: base url for rel=canonical
 
+	mux.Handle("GET /{$}", &websiteHomeHandler{
+		websiteHandlerBase: base,
+	})
 	mux.Handle("/static/", static.Handler(static.Website))
 
 	return commonMiddleware(mux), nil
+}
+
+type websiteHandlerBase struct {
+	Host string
+	Data func() (ottrecidx.DataRef, bool)
+}
+
+func (h *websiteHandlerBase) render(w http.ResponseWriter, r *http.Request, fn func(data ottrecidx.DataRef) (c templ.Component, status int, err error)) {
+	var (
+		data ottrecidx.DataRef
+		ok   bool
+	)
+	if h.Data != nil {
+		data, ok = h.Data()
+	}
+	if !ok {
+		slog.Error("website: no data available")
+		templates.RenderError(w, r, templates.WebsiteErrorPage, "Data Unavailable", "data not available, try again later", http.StatusServiceUnavailable)
+		return
+	}
+	if err := templates.Render(w, r, templates.WebsiteErrorPage, data.Index().Hash(), func() (c templ.Component, status int, err error) {
+		return fn(data)
+	}); err != nil {
+		slog.Error("website: failed to render page", "url", r.URL.String(), "error", err)
+	}
+}
+
+type websiteHomeHandler struct {
+	websiteHandlerBase
+}
+
+func (h *websiteHomeHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Vary", "Accept-Encoding")
+
+	if r.URL.RawQuery != "" {
+		w.Header().Set("Cache-Control", "no-store")
+		http.Redirect(w, r, r.URL.EscapedPath(), http.StatusTemporaryRedirect)
+		return
+	}
+
+	h.render(w, r, func(data ottrecidx.DataRef) (templ.Component, int, error) {
+		return templates.WebsitePage(templates.WebsitePageParams{
+			Title: "test",
+		}), http.StatusOK, nil
+	})
 }
